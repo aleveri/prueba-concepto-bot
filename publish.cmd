7ee5b20ece111c9582d024c8000f7033b0a2a18f
Andrés Felipe Leveri Montaña <andresl@lagashcoldev.com>
2018-10-30 13:44:08 -0500

    init repo

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild BasicBot.sln -p:DeployOnBuild=true -p:PublishProfile=prueba-concepto-Web-Deploy.pubxml -p:Password=FjWjcu6oE3jgixibQJJ5CbaaQ14Lrayes27JzMLefYYBzQRz4idm3tzikypD
+
